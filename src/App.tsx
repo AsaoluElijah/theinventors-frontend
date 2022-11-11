5912be5cf0a76f77c4137d96af7a8b7ec0de156d
Asaolu Elijah <asaoluelijah7@gmail.com>
2022-11-11 19:45:21 +0100

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
