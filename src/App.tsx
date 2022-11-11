6f1db25f06bba8dc510a7ba77e6ae7745818f6af
olawanlejoel <info4tobest@gmail.com>
2022-11-11 20:06:03 +0100

    fix merge issues manually

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
-import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 const App = () => {
 	return (
 		<>
-			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 			</Routes>
