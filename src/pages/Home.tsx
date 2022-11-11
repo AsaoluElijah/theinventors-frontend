b384362e1b296e0ee6f2c35dc08d3966b358258a
Asaolu Elijah <asaoluelijah7@gmail.com>
2022-11-11 19:44:38 +0100

    modified nav position

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,9 @@
 import Hero from "../components/Hero";
 
-import { useState } from "react";
-
 const Home = () => {
-  const todos = "Hello";
   return (
     <div>
-      Home
-      <Hero todos={todos} />
+      <Hero />
     </div>
   );
 };
