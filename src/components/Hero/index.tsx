9f90747efdecba7c50004db2f6cd927142aa4cb1
olawanlejoel <info4tobest@gmail.com>
2022-11-15 12:13:22 +0100

    ehnanced styling

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,31 +1,42 @@
-import "./index.scss";
-import NavbarTransparent from "../NavbarTransparent";
+import './index.scss';
+import NavbarTransparent from '../NavbarTransparent';
+import { RoughNotation } from 'react-rough-notation';
 
 const Hero = () => {
-  return (
-    <section className="hero-container">
-      <NavbarTransparent />
-      <div className="hero-section">
-        <div style={{ marginBottom: "10vh" }}>
-          {/* TECH - DESIGN, DEV, ML, DS */}
+	return (
+		<section className="hero-container">
+			<NavbarTransparent />
+			<div className="hero-section_container container">
+				<div className="hero-section">
+					{/* TECH - DESIGN, DEV, ML, DS */}
 
-          <h2>Accelerate ✨</h2>
-          <h1>
-            Your <span className="text-purple">TECH</span> Journey
-          </h1>
+					<div>
+						<h2>Accelerate ✨</h2>
+						<h1>
+							Your{' '}
+							<RoughNotation
+								type="highlight"
+								show={true}
+								color="#B6A4FF"
+								strokeWidth={3}
+							>
+								Tech
+							</RoughNotation>{' '}
+							Journey
+						</h1>
+					</div>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
-            natus deleniti, eius reiciendis error ullam <br /> odio quo optio
-            similique cum quae consequuntur minima accusantium iusto iste eaque
-            cupiditate quos veniam?
-          </p>
-          <br />
-          <button className="btn btn-purple">Get Started</button>
-        </div>
-      </div>
-    </section>
-  );
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
+						natus deleniti, eius reiciendis error ullam <br /> odio quo optio
+						similique cum quae consequuntur minima accusantium iusto iste eaque
+						cupiditate quos veniam?
+					</p>
+					<button className="btn btn-purple">Get Started</button>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Hero;
