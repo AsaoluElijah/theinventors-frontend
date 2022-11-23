378fd24110ef5b09fcb0412ad0bcc1d24845f674
Asaolu Elijah <asaoluelijah7@gmail.com>
2022-11-23 16:56:25 +0100

    updated hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,49 +1,58 @@
-import './index.scss';
-import NavbarTransparent from '../NavbarTransparent';
-import { RoughNotation } from 'react-rough-notation';
+import "./index.scss";
+import NavbarTransparent from "../NavbarTransparent";
+import { RoughNotation } from "react-rough-notation";
 
-import member3 from '../../images/react-icon.png';
+import member3 from "../../images/react-icon.png";
 
 const Hero = () => {
-	return (
-		<section className="hero-container">
-			<div className="circle">
-				<div className="circle-container">
-					<img src={member3} alt="" />
-				</div>
-			</div>
-			<NavbarTransparent />
-			<div className="hero-section_container container">
-				<div className="hero-section">
-					{/* TECH - DESIGN, DEV, ML, DS */}
+  return (
+    <section className="hero-container">
+      <div className="circle d-sm-none">
+        <div className="circle-container">
+          <img
+            src={member3}
+            alt=""
+            style={{
+              width: "70px",
+              height: "70px",
+            }}
+          />
+        </div>
+      </div>
+      <NavbarTransparent />
+      <div className="hero-section_container container">
+        <div className="hero-section">
+          {/* TECH - DESIGN, DEV, ML, DS */}
 
-					<div>
-						<h2>Accelerate ✨</h2>
-						<h1>
-							Your{' '}
-							<RoughNotation
-								type="highlight"
-								show={true}
-								color="#B6A4FF"
-								strokeWidth={3}
-							>
-								Tech
-							</RoughNotation>{' '}
-							Journey
-						</h1>
-					</div>
+          <div>
+            <h1>
+              Accelerate <span className="d-sm-none">✨</span>
+              <br className="d-sm-none" />
+              <RoughNotation
+                type="underline"
+                show={true}
+                color="#B6A4FF"
+                strokeWidth={3}
+              >
+                Your&nbsp;&nbsp;tech&nbsp;
+              </RoughNotation>{" "}
+              <br className="d-sm-block" />
+              Journey
+            </h1>
+          </div>
 
-					<p>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
-						natus deleniti, eius reiciendis error ullam <br /> odio quo optio
-						similique cum quae consequuntur minima accusantium iusto iste eaque
-						cupiditate quos veniam?
-					</p>
-					<button className="btn btn-purple">Get Started</button>
-				</div>
-			</div>
-		</section>
-	);
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
+            natus deleniti, eius reiciendis error ullam{" "}
+            <br className="d-sm-none" /> odio quo optio similique cum quae
+            consequuntur minima accusantium iusto iste eaque cupiditate quos
+            veniam?
+          </p>
+          <button className="btn btn-purple">Get Started</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/NavbarTransparent/index.tsx
+++ src/components/NavbarTransparent/index.tsx
@@ -1,29 +1,29 @@
-import './index.scss';
+import "./index.scss";
 
 const NavbarTransparent = () => {
-	return (
-		<nav className="navbar container">
-			<div>
-				<a href="" className="nav-brand">
-					<img src={require('../../assets/logo.png')} alt="Inventors Logo" />
-				</a>
-			</div>
-			<ul className="nav-items">
-				<li>
-					<a href="#about-us">About Us</a>
-				</li>
-				<li>
-					<a href="#events">Events</a>
-				</li>
-				<li>
-					<a href="#impacts">Our Impacts</a>
-				</li>
-			</ul>
-			<div>
-				<a href="">{/* <button>Get Started ?</button> */}</a>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar container">
+      <div>
+        <a href="" className="nav-brand">
+          <img src={require("../../assets/logo.png")} alt="Inventors Logo" />
+        </a>
+      </div>
+      <ul className="d-sm-none nav-items">
+        <li>
+          <a href="#about-us">About Us</a>
+        </li>
+        <li>
+          <a href="#events">Events</a>
+        </li>
+        <li>
+          <a href="#impacts">Our Impacts</a>
+        </li>
+      </ul>
+      <div>
+        <a href="">{/* <button>Get Started ?</button> */}</a>
+      </div>
+    </nav>
+  );
 };
 
 export default NavbarTransparent;
